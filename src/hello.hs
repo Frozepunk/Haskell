1c07ef2411454ff4b1374180ffc3371829470288
Frozepunk <smg2750935@gmail.com>
2024-11-27 21:30:50 +0530

    ntg

--- src/hello.hs
+++ src/hello.hs
@@ -1,6 +1,5 @@
-hello="hello world"
-world="This is how you print multiple lines in haskell"
 -- we can use a looping statement to print multiple lines of print statements
 main= do
-    print hello
-    print world
+    print "hello"
+    print "world"
+    print "This is also printing statement"
