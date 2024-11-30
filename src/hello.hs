0205e56640d2eb0984618c25a80db8a640b846ae
Frozepunk <smg2750935@gmail.com>
2024-11-30 20:34:42 +0530

    rebaseing the code base

--- src/hello.hs
+++
@@ -1,7 +0,0 @@
--- we can use a looping statement to print multiple lines of print statements
-main= do
-    print "hello"
-    print "world"
-    print "This is also printing statement"
-    putStrLn "hello world"
--- this is also one of the printing statement in haskelli
